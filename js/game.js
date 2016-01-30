de7bf78447ad18287dac46506221a29b75a41a7f
Omar Elsharkawy <omar.s.elsharkawy@gmail.com>
2016-01-30 11:47:12 -0800

    Created Game Initial Structure.

--- js/game.js
+++ js/game.js
@@ -1,1 +1,17 @@
 alert('The Doctor');
+//Creates game window dimension and Auto render.
+var game = new Phaser.Game(640,360, Phaser.AUTO);
+var GameState = {
+  preload: function(){
+
+  },
+  create: function(){
+
+  },
+  update: function(){
+
+  }
+};
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
