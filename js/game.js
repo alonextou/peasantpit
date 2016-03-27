c96c398db01ec01a1ca08bfbb9b9e88336ff37c3
Alex Crawford <alex.crawford@codingzeal.com>
2016-03-26 19:13:44 -0700

    Sheep thrusting and stopping on collision

--- js/game.js
+++ js/game.js
@@ -3,23 +3,26 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 function preload() {
 
     game.load.image('stars', 'assets/images/background.jpg');
-    game.load.image('ship', 'assets/images/player.png');
-    game.load.image('panda', 'assets/images/sheep.png');
-    game.load.image('sweet', 'assets/images/sheep.png');
+    game.load.image('player', 'assets/images/player.png');
+    game.load.image('playerArms', 'assets/images/player.png');
+    game.load.image('sheep1', 'assets/images/sheep.png');
+    game.load.image('sheep2', 'assets/images/sheep.png');
     game.load.image('campfire', 'assets/images/campfire.png');
 
-    //this.game.load.physics("player_physics", "assets/player_physics.json");
-
+    game.load.physics("playerPhysics", "assets/player_collision.json");
 }
 
 var starfield;
 var campfire;
-var ship;
-var panda;
-var sweet;
+var player;
+var playerArms;
+var sheep1;
+var sheep2;
 var cursors;
 
 function create() {
+
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
     game.physics.startSystem(Phaser.Physics.P2JS);
     game.physics.p2.restitution = .1;
@@ -30,42 +33,40 @@ function create() {
     // campfire
     campfire = game.add.sprite(game.world.centerX, game.world.centerY, 'campfire');
     campfire.anchor.setTo(.5);
-    //game.physics.enable(campfire, Phaser.Physics.ARCADE);
     game.physics.p2.enable(campfire, false);
     campfire.body.setCircle(40, 0, 0, 0);
 
-    // panda
-    panda = game.add.sprite(game.world.randomX, game.world.randomY, 'panda');
-    game.physics.p2.enable(panda, false);
-    panda.body.setCircle(30, 0, 0, 0);
-    panda.body.damping = .99;
-    panda.body.angularDamping = 1;
+    // sheep1
+    sheep1 = game.add.sprite(game.world.randomX, game.world.randomY, 'sheep1');
+    game.physics.p2.enable(sheep1, true);
+    sheep1.body.setCircle(30, 0, 0, 0);
+    sheep1.body.damping = .99;
+    sheep1.body.angularDamping = 1;
 
-    // sweet
-    sweet = game.add.sprite(game.world.randomX, game.world.randomY, 'sweet');
-    game.physics.p2.enable(sweet, false);
-    sweet.body.setCircle(30, 0, 0, 0);
-    sweet.body.damping = .99;
-    sweet.body.angularDamping = 1;
+    // sheep2
+    sheep2 = game.add.sprite(game.world.randomX, game.world.randomY, 'sheep2');
+    game.physics.p2.enable(sheep2, true);
+    sheep2.body.setCircle(30, 0, 0, 0);
+    sheep2.body.damping = .99;
+    sheep2.body.angularDamping = 1;
 
-    ship = game.add.sprite(200, 200, 'ship');
+    // player
+    player = game.add.sprite(200, 200, 'player');
+    player.anchor.setTo(.5);
+    game.physics.p2.enable(player, true);
+    player.body.setCircle(30, 0, 40, 0);
+    player.body.damping = 1;
 
-    //  Create our physics body - a 28px radius circle. Set the 'false' parameter below to 'true' to enable debugging
-    game.physics.p2.enable(ship, false);
-    //ship.body.loadPolygon("player_physics", "ship");
-    //ship.body.loadPolygon("player_physics", "player");
-    ship.body.setCircle(30);
-    ship.body.fixedRotation = false;
-    ship.body.damping = 1;
+    // playerArms
+    playerArms = game.add.sprite(200, 200, 'playerArms');
+    game.physics.p2.enable(playerArms, true);
+    playerArms.body.clearShapes();
+    playerArms.body.loadPolygon('playerPhysics', 'playerArms');
+    playerArms.body.damping = 1;
+    playerArms.alpha = 0;
 
-    game.camera.follow(ship);
 
-    //  Here we create a Body specific callback.
-    //  Note that only impact events between the ship and the panda are used here, the sweet/candy object is ignored.
-    ship.body.createBodyCallback(panda, hitPanda, this);
-
-    //  And before this will happen, we need to turn on impact events for the world
-    //game.physics.p2.setImpactEvents(true);
+    game.camera.follow(player);
 
     game.physics.p2.setPostBroadphaseCallback(collideCampfire, this);
 
@@ -73,80 +74,89 @@ function create() {
 
 }
 
-function hitPanda(body1, body2) {
-
-    //  body1 is the space ship (as it's the body that owns the callback)
-    //  body2 is the body it impacted with, in this case our panda
-    //  As body2 is a Phaser.Physics.P2.Body object, you access its owner (the sprite) via the sprite property:
-    //body2.sprite.alpha -= 0.1;
-
-}
-
 function spriteBurning (object) {
   console.log('player damage');
-  console.log(object)
   eval(object).tint = 0xff0000;
+  eval(object).body.thrust(-100);
 }
 
 function collideCampfire (body1, body2) {
+    var hasPlayer = (body1.sprite.key === 'player' || body2.sprite.key === 'player');
+    var hasPlayerArms = (body1.sprite.key === 'playerArms' || body2.sprite.key === 'playerArms');
+    var hasCampfire = (body1.sprite.key === 'campfire' || body2.sprite.key === 'campfire');
+
+    //if (hasCampfire) console.log('campfire')
+    if (body1.sprite.key === 'sheep1') {
+      eval(body1.sprite.key).body.thrust(0);
+      console.log('sheep1')
+    } else if (body2.sprite.key === 'sheep2') {
+      eval(body2.sprite.key).body.thrust(0);
+      console.log('sheep2');
+    }
+
+    if (hasPlayer) {
+      if (hasCampfire) {
+        spriteBurning('player')
+        return false;
+      }
+      if (hasPlayerArms) {
+        return false;
+      }
+    }
+
     if (body1.sprite.key === 'campfire') {
-      spriteBurning(body2.sprite.key)
+      spriteBurning(body2.sprite.key);
       return false;
     } else if (body2.sprite.key === 'campfire') {
-      spriteBurning(body1.sprite.key)
+      spriteBurning(body1.sprite.key);
       return false;
     }
+
     return true;
 }
 
 function update() {
 
-    ship.tint = 0xffffff;
-    panda.tint = 0xffffff;
-    sweet.tint = 0xffffff;
+    player.tint = 0xffffff;
+    sheep1.tint = 0xffffff;
+    sheep2.tint = 0xffffff;
 
-    //panda.body.setZeroRotation();
-    //game.physics.p2.overlap(campfire, ship, playerBurning);
-
-    /*
-    var bodyA = game.physics.p2.getBody(ship);
-    var bodyB = game.physics.p2.getBody(campfire);
-
-    if(p2.Broadphase.aabbCheck(bodyA,bodyB)){
-      console.log("ok");
-    }
-    */
-
+    sheep1.body.thrust(100);
+    sheep2.body.thrust(100);
 
     if (cursors.left.isDown)
     {
-        ship.body.rotateLeft(90);
+        player.body.rotateLeft(90);
     }
     else if (cursors.right.isDown)
     {
-        ship.body.rotateRight(90);
+        player.body.rotateRight(90);
     } else {
-      ship.body.setZeroRotation();
+      player.body.setZeroRotation();
     }
 
     if (cursors.up.isDown)
     {
-        ship.body.thrust(30000);
+        player.body.thrust(30000);
     }
     else if (cursors.down.isDown)
     {
-        ship.body.thrust(-30000);
+        player.body.thrust(-30000);
     }
 
     if (!game.camera.atLimit.x)
     {
-        starfield.tilePosition.x += (ship.body.velocity.x * 16) * game.time.physicsElapsed;
+        starfield.tilePosition.x += (player.body.velocity.x * 16) * game.time.physicsElapsed;
     }
 
     if (!game.camera.atLimit.y)
     {
-        starfield.tilePosition.y += (ship.body.velocity.y * 16) * game.time.physicsElapsed;
+        starfield.tilePosition.y += (player.body.velocity.y * 16) * game.time.physicsElapsed;
     }
+
+    playerArms.body.x = player.body.x
+    playerArms.body.angle = player.body.angle
+    playerArms.body.y = player.body.y
 
 }
 
